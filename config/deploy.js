722a08a72d8b5f6dce32ff7f7c8296a1322d6ce2
D3AH <diegoeauyonh@gmail.com>
2018-10-23 15:12:16 -0600

    Cambiando el 'worktreePath' en config/deploy.js.

--- config/deploy.js
+++ config/deploy.js
@@ -24,7 +24,7 @@ module.exports = function(deployTarget) {
     ENV.git = {
       repo: 'https://github.com/RedCiudadana/EleccionCGC',
       branch: 'gh-pages',
-      worktreePath: '/tmp/deploy'
+      worktreePath: '/tmp/ember-deploy'
     };
   }
 
